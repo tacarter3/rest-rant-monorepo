ee448adf4bad77ddb71614b702921ddbbe09ee63
Timothy Carter <talic2002@gmail.com>
2022-05-16 17:52:19 -0400

    Completed jwt authentication

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
 
 export const CurrentUser = createContext()
@@ -6,6 +7,20 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5004/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+
     window.setCurrentUser = setCurrentUser
 
     return (
